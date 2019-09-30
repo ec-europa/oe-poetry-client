9e27652de39048ada54fcbba574554ccef569300
D Vargas <hiphipvargas@sapo.pt>
2019-09-30 17:01:02 +0200

    OPENEUROPA-2251: Allow to add languages for a request.

---
+++ src/Messages/Requests/AddLanguagesRequest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace EC\Poetry\Messages\Requests;
+
+use EC\Poetry\Messages\Components\Identifier;
+use EC\Poetry\Messages\Traits\WithDetailsTrait;
+use EC\Poetry\Messages\Traits\WithReferenceDocumentsTrait;
+use EC\Poetry\Messages\Traits\WithReturnAddressTrait;
+use EC\Poetry\Messages\Traits\WithSourceTrait;
+use EC\Poetry\Messages\Traits\WithTargetsTrait;
+use EC\Poetry\Services\Settings;
+use Symfony\Component\Validator\Mapping\ClassMetadata;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Add languages to a previous translation request.
+ *
+ * @package EC\Poetry\Messages\Requests
+ */
+class AddLanguagesRequest extends AbstractRequest
+{
+    use WithDetailsTrait;
+    use WithReturnAddressTrait;
+    use WithTargetsTrait;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __construct(Identifier $identifier, Settings $settings)
+    {
+        parent::__construct($identifier, $settings);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getType()
+    {
+        return self::REQUEST_NEW_POST;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getConstraints(ClassMetadata $metadata)
+    {
+        parent::getConstraints($metadata);
+        $metadata->addPropertyConstraint('targets', new Assert\Valid(['traverse' => true]));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTemplate()
+    {
+        return 'messages::add-languages';
+    }
+}
---
+++ templates/messages/add-languages.tpl.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @file
+ * Template file.
+ *
+ * @var \EC\Poetry\Services\Plates\Template $this
+ * @var \EC\Poetry\Messages\Requests\CreateTranslationRequest $message
+ * @var string $identifier
+ */
+?>
+<?php $this->layout('layout', ['identifier' => $identifier, 'type' => $message->getType(), 'communication' => $message->getCommunication()]) ?>
+<?= $this->component($message->getIdentifier()); ?>
+<?php if ($message->getDetails()) : ?>
+    <?= $this->component($message->getDetails()); ?>
+<?php endif; ?>
+<?php if ($message->getTargets()) : ?>
+    <?php foreach ($message->getTargets() as $target) : ?>
+        <?= $this->component($target); ?>
+    <?php endforeach; ?>
+<?php endif; ?>
