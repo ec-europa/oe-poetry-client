65114d3fbff8555f5d7ffd17fa9e665069fcfede
upchuk <danny@webomelette.com>
2020-02-22 10:52:36 +0100

    OPENEUROPA-2251: Minor comment fix.

--- templates/messages/add-languages.tpl.php
+++ templates/messages/add-languages.tpl.php
@@ -4,7 +4,7 @@
  * Template file.
  *
  * @var \EC\Poetry\Services\Plates\Template $this
- * @var \EC\Poetry\Messages\Requests\CreateTranslationRequest $message
+ * @var \EC\Poetry\Messages\Requests\AddLanguagesRequest $message
  * @var string $identifier
  */
 ?>
